ebafb177bbda18479b67c0094e04a9a01eb228e0
y-nagamoto <thegameisafoot1854@gmail.com>
2021-01-25 01:29:50 +0900

    変数宣言、型変換

--- 1-1.hensu.sengen.py
+++ 1-1.hensu.sengen.py
@@ -1,5 +1,21 @@
 num = 1
 name = 'Mike'
+is_ok = True
 
 print(num, type(num))
 print(name, type(name))
+print(is_ok, type(is_ok))
+"""
+1 <class 'int'>
+Mike <class 'str'>
+True <class 'bool'>
+"""
+
+
+#型変換
+name = '1'
+new_name = int(name)
+print(new_name, type(new_name))
+"""
+1 <class 'int'>
+"""
