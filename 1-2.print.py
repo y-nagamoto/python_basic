34da8cb728934a48e81cebc91fe2c2d957610bed
y-nagamoto <thegameisafoot1854@gmail.com>
2021-01-25 01:54:14 +0900

    1-3まで実施

---
+++ 1-2.print.py
@@ -0,0 +1,6 @@
+print ('Hi', 'Mike', sep=',')
+"""
+Hi,Mike
+"""
+
+print ('Hi', 'Mike', sep=',', end='.\n')
---
+++ 1-3.number.py
@@ -0,0 +1,40 @@
+print(50 - 5 * 6)
+# 20
+
+print((50 - 5) * 6)
+# 270
+
+print(8 / 5)
+# 1.6
+
+print(type(1))
+# <class 'int'>
+
+print(type(1.6))
+# <class 'float'>
+
+print(17 / 3)
+# 5.666666666666667
+
+print(17 // 3)
+# 5
+
+print(17 % 3)
+# 2 （余剰）
+
+print(5 ** 2)
+# 25
+
+x = 17 / 3
+print(round(x, 2))
+# 5.67  (丸め)
+
+import math
+result = math.sqrt(25)
+print(result)
+# 5.0
+
+print(help(math))
+"""
+math関数のメソッドが見れる
+"""
